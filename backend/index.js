7affa611e7ff86b437965b29313cf87489fab43b
Andrew Husiev <andrew.husiev@gmail.com>
2025-11-07 10:34:28 +0100

    Add endpoints and simple cache to keep pokemons data

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,63 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+
 const port = process.env.PORT || 3000;
 const pokemonApi = process.env.POKEMON_API_URL;
+
+const pokemonListCache = {
+  data: null,
+  timestamp: null,
+};
+
+const validateCache = () => {
+  if (!pokemonListCache.timestamp) return false;
+
+  const now = Date.now();
+  const age = now - pokemonListCache.timestamp;
+
+  return age < 24 * 60 * 60 * 1000; // 24h
+};
+
+async function fetchPokemonList() {
+  const response = await fetch(`${pokemonApi}/pokemon`);
+  const responseJson = await response.json();
+  return responseJson;
+}
+
+async function fetchSinglePokemon(name) {
+  const response = await fetch(`${pokemonApi}/pokemon/${name}`);
+  const responseJson = await response.json();
+  return responseJson;
+}
+
+app.get('/api/pokemons', async (req, res) => {
+  try {
+    const isCacheValid = validateCache();
+
+    if (!isCacheValid) {
+      pokemonListCache.data = await fetchPokemonList();
+      pokemonListCache.timestamp = Date.now();
+    }
+    res.json(pokemonListCache);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Failed to fetch pokemons' });
+  }
+});
+
+app.get('/api/pokemons/:name', async (req, res) => {
+  const { name } = req.params;
+
+  try {
+    const response = await fetchSinglePokemon(name);
+    res.json(response);
+  } catch (err) {
+    res.status(404).json({ error: 'Pokemon not found' });
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Express backend up and maybe even running');
